c8a8fd66ee332b054aece5cb6dd987581917db2d
上原理史 <masashi@ueharasatoshinoMacBook-Air.local>
2019-07-04 14:59:26 +0900

    ユーザー更新、削除機能追加

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"net/http"
+	"strconv"
+
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/masashi1014/go_sample/model"
@@ -10,10 +13,105 @@ func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("template/*.html")
 
+	//SELECE ALL(ユーザー一覧)
 	router.GET("/", func(ctx *gin.Context) {
 		users := model.GetAllUsers()
-		ctx.HTML(200, "users.html", gin.H{
+		ctx.HTML(200, "index.html", gin.H{
 			"users": users,
+		})
+	})
+
+	//INSERT(ユーザー新規作成)
+	router.POST("/new", func(ctx *gin.Context) {
+
+		//バリデーション
+		var form model.User
+		if err := ctx.Bind(&form); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"status": "BadRequest", "error": err})
+			return
+		}
+
+		name := ctx.PostForm("name")
+		password := ctx.PostForm("password")
+		email := ctx.PostForm("email")
+		new_user := model.User{Name: name, Password: password, Email: email}
+		model.CreateUser(new_user)
+		message := "作成しました"
+		users := model.GetAllUsers()
+		ctx.HTML(200, "index.html", gin.H{
+			"users":   users,
+			"message": message,
+		})
+	})
+
+	//SELECT WHERE(ユーザー詳細画面)
+	router.GET("/detail/:id", func(ctx *gin.Context) {
+		n := ctx.Param("id")
+		id, err := strconv.Atoi(n)
+		if err != nil {
+			panic(err)
+		}
+		user := model.GetUserDetail(id)
+		ctx.HTML(200, "detail.html", gin.H{
+			"user": user,
+			"id":   id,
+		})
+	})
+
+	//UPDATE(ユーザー更新)
+	router.POST("/update/:id", func(ctx *gin.Context) {
+
+		//バリデーション
+		var form model.User
+		if err := ctx.Bind(&form); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"status": "BadRequest", "error": err})
+			return
+		}
+
+		n := ctx.Param("id")
+		id, err := strconv.Atoi(n)
+		if err != nil {
+			panic(err)
+		}
+		name := ctx.PostForm("name")
+		password := ctx.PostForm("password")
+		email := ctx.PostForm("email")
+		update_user := model.User{Name: name, Password: password, Email: email}
+		model.UpdateUser(id, update_user)
+		message := "更新しました"
+		ctx.HTML(200, "detail.html", gin.H{
+			"user":    update_user,
+			"message": message,
+		})
+	})
+
+	//DELETE確認画面表示
+	router.GET("/delete/:id", func(ctx *gin.Context) {
+		n := ctx.Param("id")
+		id, err := strconv.Atoi(n)
+		if err != nil {
+			panic(err)
+		}
+		user := model.GetUserDetail(id)
+		ctx.HTML(200, "delete.html", gin.H{
+			"user": user,
+			"id":   id,
+		})
+	})
+
+	//DELETE(ユーザー削除)
+	router.POST("/delete/:id", func(ctx *gin.Context) {
+		n := ctx.Param("id")
+		id, err := strconv.Atoi(n)
+		if err != nil {
+			panic(err)
+		}
+		model.DeleteUser(id)
+		message := "削除しました"
+		users := model.GetAllUsers()
+		ctx.HTML(200, "index.html", gin.H{
+			"users":   users,
+			"message": message,
 		})
 	})
 
--- model/user.go
+++ model/user.go
@@ -7,18 +7,97 @@ import (
 type User struct {
 	gorm.Model
 	Name     string `gorm:"size:255"`
-	Password string `gorm:"size:255"`
+	Password string `form:"password" binding:"required" gorm:"size:255"` //パスワード入力必須
 	Email    string `gorm:"size:255"`
 }
 
+//データベース接続
+func dbInit() *gorm.DB {
+	db, err := gorm.Open("mysql", "root:root@tcp(localhost:8889)/go_test")
+	defer db.Close()
+	if err != nil {
+		panic(err.Error())
+	}
+	
+	// マイグレーション(テーブルがなかったら自動生成)
+    db.AutoMigrate(&User{})
+	return db
+}
+
+//ユーザー全権取得
 func GetAllUsers() []User {
 	dbInit()
 	db, err := gorm.Open("mysql", "root:root@tcp(localhost:8889)/go_test?parseTime=true")
+	defer db.Close()
 	if err != nil {
-        panic("データベース開けず！（dbDelete)")
-    }
+		panic("DB open failed")
+	}
 	user := []User{}
 	db.Find(&user)
 
 	return user
+}
+
+//ユーザー1件取得
+func GetUserDetail(id int) User {
+	dbInit()
+	db, err := gorm.Open("mysql", "root:root@tcp(localhost:8889)/go_test?parseTime=true")
+	defer db.Close()
+	if err != nil {
+		panic("DB open failed")
+	}
+	user := User{}
+	db.First(&user, id)
+
+	return user
+}
+
+//ユーザー新規作成
+func CreateUser(user User) {
+	dbInit()
+	db, err := gorm.Open("mysql", "root:root@tcp(localhost:8889)/go_test?parseTime=true")
+	defer db.Close()
+	if err != nil {
+		panic("DB open failed")
+	}
+	rec := User{
+		Name:     user.Name,
+		Password: user.Password,
+		Email:    user.Email,
+	}
+
+	db.NewRecord(&rec)
+	db.Create(&rec)
+}
+
+//ユーザー更新
+func UpdateUser(id int, update_user User) {
+	dbInit()
+	db, err := gorm.Open("mysql", "root:root@tcp(localhost:8889)/go_test?parseTime=true")
+	defer db.Close()
+	if err != nil {
+		panic("DB open failed")
+	}
+
+	user := User{}
+	db.First(&user, id)
+	user.Name = update_user.Name
+	user.Password = update_user.Password
+	user.Email = update_user.Email
+
+	db.Save(&user)
+}
+
+//ユーザー削除
+func DeleteUser(id int) {
+	dbInit()
+	db, err := gorm.Open("mysql", "root:root@tcp(localhost:8889)/go_test?parseTime=true")
+	defer db.Close()
+	if err != nil {
+		panic("DB open failed")
+	}
+
+	user := User{}
+	db.First(&user, id)
+	db.Delete(&user)
 }
